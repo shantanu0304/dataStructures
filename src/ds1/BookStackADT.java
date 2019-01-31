abfd0a46a43f4c4905661ebd6bd680dcf47e1dca
shantanu0304 <shantanumittal3499@gmail.com>
2019-01-31 09:25:52 +0530

    book class constructed

---
+++ src/ds1/BookStackADT.java
@@ -0,0 +1,48 @@
+package ds1;
+
+class book{
+    private String bookName;
+    private String bookAuthor;
+    private int bookEdition;
+    private double bookPrice;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getBookEdition() {
+        return bookEdition;
+    }
+
+    public void setBookEdition(int bookEdition) {
+        this.bookEdition = bookEdition;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "( " + bookName + ", " + bookAuthor + ", " + bookEdition+ ", " + bookPrice + " )";
+    }
+}
+
+public class BookStackADT {
+}
