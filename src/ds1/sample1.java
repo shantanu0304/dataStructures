3f9da1e19555519ce63c5937bcadd560994658bf
Shantanu <shantanumittal3499@gmail.com>
2019-02-12 21:35:19 +0530

    sample file

---
+++ src/ds1/sample1.java
@@ -0,0 +1,40 @@
+package ds1;
+
+import java.util.Date;
+
+public class sample1 {
+    public static void main(String[] args) {
+
+/*
+        int[] a = new int[10];
+        int[] b = new int[10];
+
+        for (int i = 0; i < 10 ; i++) {
+            a[i] = i*i;
+
+        }
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(a[i]);
+
+        }
+
+        int j = 0;
+        for (int i : b ) {
+            i = j*j;
+            b[j] = i;
+            j++;
+        }
+
+        for (int i : b)
+        {
+            System.out.println(i);
+        }*/
+
+        Date[] date = new Date[10];
+
+        for (Date date1 : date )
+        {
+            System.out.println(date1);
+        }
+    }
+}
