30f31b576ce7d340f8b090164746f8d92b8d2f93
Shantanu <shantanumittal3499@gmail.com>
2019-02-12 22:13:58 +0530

    sample file updated1

--- src/ds1/sample1.java
+++ src/ds1/sample1.java
@@ -1,6 +1,7 @@
 package ds1;
 
 import java.util.Date;
+import java.util.Random;
 
 public class sample1 {
     public static void main(String[] args) {
@@ -40,6 +41,58 @@ public class sample1 {
             date[i] = new Date(2019-1900 , 12-1, i+1 );
             System.out.println(date[i]);
         }*/
+
+        int[][] arr2d = new int[20][20];
+        int[] rowsum = new int[20];
+        int[] colsum = new int[20];
+        Random rand = new Random();
+        int x = 0;
+        for (int i = 0; i <20 ; i++)
+        {
+            for (int j = 0; j <20 ; j++) {
+                arr2d[i][j] = x;
+                if(x>100)
+                    x = 0;
+                else
+                    x++;
+                System.out.print(arr2d[i][j] + "  ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i <20 ; i++)
+        {
+            for (int j = 0; j <20 ; j++) {
+                rowsum[i] += arr2d[i][j];
+            }
+        }
+
+        for (int i = 0; i <20 ; i++)
+        {
+            for (int j = 0; j <20 ; j++) {
+                colsum[i] += arr2d[j][i];
+            }
+        }
+
+        int rowmax = rowsum[0];
+        int rowind = 0;
+        int colmax = colsum[0];
+        int colind = 0;
+        for (int i = 0; i <20 ; i++)
+        {
+            if(rowmax<rowsum[i]) {
+                rowmax = rowsum[i];
+                rowind = i;
+            }
+
+            if (colmax<colsum[i]) {
+                colmax = colsum[i];
+                colind = i;
+            }
+        }
+
+
+        System.out.println(colind + " " + rowind);
 
 
 
