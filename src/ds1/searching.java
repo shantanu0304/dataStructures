b6ed8ac793ca01d935d008a81aebc05a06eec3e8
Shantanu <shantanumittal3499@gmail.com>
2019-02-13 20:51:56 +0530

    updated searching

---
+++ src/ds1/searching.java
@@ -0,0 +1,44 @@
+package ds1;
+
+//import java.util.Random;
+
+/*class list{
+    private int strength;
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public int toString() {
+        return  strength;
+    }
+}*/
+
+public class searching {
+
+    //list[] secg = new list[40];
+
+    public static void main(String[] args) {
+        /*int[] arr = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] == 30){
+                System.out.println("the element is found at "+i);
+                break;
+            }
+        }*/
+
+        /*searching search = new searching();*/
+
+
+    }
+
+}
