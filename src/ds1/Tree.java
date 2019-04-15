6b256e15c1c6bbf909f6f4cbb7c916d545f934b9
Shantanu <shantanumittal3499@gmail.com>
2019-04-15 23:22:06 +0530

    Added traversal methods in tree implementation

--- src/ds1/Tree.java
+++ src/ds1/Tree.java
@@ -42,6 +42,42 @@ class bst{
         return node;
 
     }
+
+    public void preorder(Node node){
+        if(node == null){
+            return;
+        }
+
+        System.out.print(node.data+" ");
+
+        preorder(node.left);
+
+        preorder(node.right);
+
+    }
+
+    public void inorder(Node node){
+        if(node == null){
+            return;
+        }
+        inorder(node.left);
+
+        System.out.print(node.data+" ");
+
+        inorder(node.right);
+
+    }
+
+    public void postorder(Node node){
+        if(node == null){
+            return;
+        }
+        postorder(node.left);
+
+        postorder(node.right);
+
+        System.out.print(node.data+" ");
+    }
 }
 
 public class Tree
@@ -56,11 +92,21 @@ public class Tree
         root = t1.insert(root,6);
         root = t1.insert(root,1);
         root = t1.insert(root,4);
-        root = t1.insert(root,10);
-        root = t1.insert(root,14);
         root = t1.insert(root,13);
+        root = t1.insert(root,14);
+        root = t1.insert(root,10);
 
         System.out.println(root);
+
+        t1.preorder(root);
+
+        System.out.println();
+
+        t1.inorder(root);
+
+        System.out.println();
+
+        t1.postorder(root);
     }
 
 }
